2480ad011feb4c349c89b987fb9e1ac55dd86c59
AnanyaA148 <138643111+AnanyaA148@users.noreply.github.com>
2024-10-19 15:24:09 -0400

    IT WORKS
    I sent an email to myself to figure out how to use the win32 library. Had to download outlook desktop. Then I tried sending the message I intend to send using the excel sheet and it sent my personalized message.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from openpyxl import Workbook
 from openpyxl import load_workbook
+import win32com.client
 
 workbook = load_workbook(filename = "Physics-Test.xlsx")
 sheet = workbook.active
@@ -19,3 +20,13 @@ for count in range(1):
 workbook.save(filename= "Physics-Test.xlsx")
 
 
+
+
+outlook = win32com.client.Dispatch("Outlook.Application")
+for count2 in range(1):
+    num2 = count2 + 2
+    new_mail = outlook.CreateItem(0)
+    new_mail.To = sheet["C" + str(num2)].value
+    new_mail.Subject = "Wrong Answers"
+    new_mail.Body = sheet["F" + str(num2)].value
+    new_mail.Send()
---
+++ outlook.py
@@ -0,0 +1,13 @@
+import win32com.client
+
+outlook = win32com.client.Dispatch("Outlook.Application")
+
+new_mail = outlook.CreateItem(0)
+
+new_mail.To = "an953660@ucf.edu"
+
+new_mail.Subject = "Wrong Answers"
+
+new_mail.Body = "Emailbody"
+
+new_mail.Send()
