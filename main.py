060c460c0d9170f34f72627248681c63b87de7b1
AnanyaA148 <138643111+AnanyaA148@users.noreply.github.com>
2024-10-21 13:15:35 -0400

    rename some variables

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import win32com.client
 
 workbook = load_workbook(filename = "Physics-Test.xlsx")
 sheet = workbook.active
-
-for count in range(2):
-    num = count+2
-    wrongQ = sheet["D"+ str(num)].value
+num_students =1
+for count in range(num_students):
+    row = count+2
+    wrongQ = sheet["D"+ str(row)].value
     wrongQ = wrongQ.replace(" ", "")
     wrongQ = wrongQ.split(",")
     messages = ""
@@ -15,14 +15,14 @@ for count in range(2):
     for i in wrongQ:
         mnum = str(int(i)+1)
         messages = messages + (sheet["E" +mnum].value) + "  \n"
-    sheet["F" + str(num)] = messages
+    sheet["F" + str(row)] = messages
 workbook.save(filename= "Physics-Test.xlsx")
 
 
 
 
 outlook = win32com.client.Dispatch("Outlook.Application")
-for count2 in range(2):
+for count2 in range(num_students):
     num2 = count2 + 2
     new_mail = outlook.CreateItem(0)
     new_mail.To = sheet["C" + str(num2)].value
